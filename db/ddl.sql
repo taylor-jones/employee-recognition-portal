bf038f0d95a605809825a03a19daafc38e7e1eb3
Taylor Jones <taylorjonesdev@gmail.com>
2019-07-13 11:18:56 -0500

    Made schema a bit more explicit, and added possibility to define start and end dates for employee branch records

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,48 +1,32 @@
 /********************************************
-  account
+  TYPES
+ *******************************************/
+DROP TYPE IF EXISTS CRUD_OPERATION CASCADE;
+CREATE TYPE CRUD_OPERATION AS ENUM ('insert', 'update', 'delete');
 
-  NOTE: The name 'account' is used because the 
-  word 'user'is reserved in postgres. We could 
-  use 'users', but I generally think it's best 
-  practice to use singular table names (but, it's
-  totally fine with me if you guys want to use 
-  plural names and switch it to 'users' instead).
 
-  NOTE: I'm not sure exactly how we want to store 
-  the signature image yet. It seems we could either 
-  store it directly in the DB (which is the current
-  configuration) or we could store it elsewhere 
-  and point to that location.
-
-  NOTE: My understanding is that we could hash and 
-  salt the passwords to keep them more secure but 
-  still use a varchar as the datatype. I'm totally
-  open to better methods.
+/********************************************
+  user_account
 ********************************************/
-
-DROP TABLE IF EXISTS account CASCADE;
-
-CREATE TABLE account (
+DROP TABLE IF EXISTS user_account CASCADE;
+CREATE TABLE user_account (
   "id" INT PRIMARY KEY,
   "email" VARCHAR(255) NULL,
-  "name" VARCHAR(255) NULL,
+  "username" VARCHAR(255) NULL,
   "password" VARCHAR(255) NULL,
   "signature" BYTEA NULL,
   "is_admin" BOOLEAN NULL,
-  CHECK (is_admin OR "name" IS NOT NULL)
+  CHECK (is_admin OR "username" IS NOT NULL)
 );
 
-ALTER TABLE account OWNER TO "tttAdmin";
-GRANT ALL ON TABLE account TO "tttAdmin";
-
+ALTER TABLE user_account OWNER TO "tttAdmin";
+GRANT ALL ON TABLE user_account TO "tttAdmin";
 
 
 /********************************************
   award_type
 ********************************************/
-
 DROP TABLE IF EXISTS award_type CASCADE;
-
 CREATE TABLE award_type (
   "id" INT PRIMARY KEY,
   "name" VARCHAR(255) NOT NULL
@@ -52,13 +36,10 @@ ALTER TABLE award_type OWNER TO "tttAdmin";
 GRANT ALL ON TABLE award_type TO "tttAdmin";
 
 
-
 /********************************************
   region
 ********************************************/
-
 DROP TABLE IF EXISTS region CASCADE;
-
 CREATE TABLE region (
   "id" INT PRIMARY KEY,
   "name" VARCHAR(255) NOT NULL
@@ -68,51 +49,45 @@ ALTER TABLE region OWNER TO "tttAdmin";
 GRANT ALL ON TABLE region TO "tttAdmin";
 
 
-
 /********************************************
   employee
 ********************************************/
-
 DROP TABLE IF EXISTS employee CASCADE;
-
 CREATE TABLE employee (
   "id" INT PRIMARY KEY,
   "email" VARCHAR(255) NULL,
-  "name" VARCHAR(255) NULL
+  "first_name" VARCHAR(100) NOT NULL,
+  "last_name" VARCHAR(100) NOT NULL
 );
 
 ALTER TABLE employee OWNER TO "tttAdmin";
 GRANT ALL ON TABLE employee TO "tttAdmin";
 
 
-
 /********************************************
   employee_region
 ********************************************/
-
 DROP TABLE IF EXISTS employee_region CASCADE;
-
 CREATE TABLE employee_region (
   "employee_id" INT NOT NULL REFERENCES employee(id),
-  "region_id" INT NOT NULL REFERENCES region(id)
+  "region_id" INT NOT NULL REFERENCES region(id),
+  "begin_date" DATE NULL,  -- when the employee began working at this region
+  "end_date" DATE NULL  -- when the employee stopped working at this region
 );
 
 ALTER TABLE employee_region OWNER TO "tttAdmin";
 GRANT ALL ON TABLE employee_region TO "tttAdmin";
 
 
-
 /********************************************
   award
 ********************************************/
-
 DROP TABLE IF EXISTS award CASCADE;
-
 CREATE TABLE award (
   "id" INT PRIMARY KEY,
   "award_type_id" INT NOT NULL REFERENCES award_type(id),
   "employee_id" INT NOT NULL REFERENCES employee(id),
-  "account_id" INT NOT NULL REFERENCES account(id),
+  "user_account_id" INT NOT NULL REFERENCES user_account(id),
   "description" VARCHAR(1000) NULL,
   "awarded_date" DATE NULL,
   "awarded_time" TIME NULL
@@ -123,21 +98,12 @@ GRANT ALL ON TABLE award TO "tttAdmin";
 
 
 /********************************************
-  TYPES
- *******************************************/
-DROP TYPE IF EXISTS CRUD_OPERATION CASCADE;
-CREATE TYPE CRUD_OPERATION AS ENUM ('insert', 'update', 'delete');
-
-
-
-/********************************************
   log
  *******************************************/
 DROP TABLE IF EXISTS log CASCADE;
-
 CREATE TABLE log (
   "id" BIGINT NOT NULL PRIMARY KEY,
-  "account_id" INT NOT NULL REFERENCES account(id),
+  "user_account_id" INT NOT NULL REFERENCES user_account(id),
   "controller_class" VARCHAR(255) NULL,
   "operation" CRUD_OPERATION NOT NULL,
   "property" VARCHAR(255) NOT NULL,
