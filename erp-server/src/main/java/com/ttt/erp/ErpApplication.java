af2d69d5568b8cda764f67e1acad80f479748ac6
Taylor Jones <taylorjonesdev@gmail.com>
2019-07-16 19:15:31 -0500

    Removed unused auditing annotation

--- erp-server/src/main/java/com/ttt/erp/ErpApplication.java
+++ erp-server/src/main/java/com/ttt/erp/ErpApplication.java
@@ -2,11 +2,9 @@ package com.ttt.erp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = {"com.ttt.erp.repository"})
-@EnableJpaAuditing
 @SpringBootApplication
 public class ErpApplication {
 	public static void main(String[] args) {
