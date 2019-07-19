189adac292f7803690d244e73fbd4f52b2df317f
Taylor Jones <jonest6@oregonstate.edu>
2019-07-18 19:52:40 -0500

    Fixed issue with retrieving embedded arrays from hal+json response

--- erp-client/src/app/services/employee/employee.service.ts
+++ erp-client/src/app/services/employee/employee.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Employee } from '../../models/employee.model';
 
 @Injectable({
@@ -12,9 +13,13 @@ export class EmployeeService {
 
 	getEmployeeById(id: number): Observable<Employee> {
 		return this.httpClient.get<Employee>(`/api/employees/${id}`);
-	}
-
-	getAllEmployees(): Observable<Employee[]> {
-		return this.httpClient.get<Employee[]>(`/api/employees`);
-	}
+  }
+  
+  getAllEmployees(): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>('/api/employees').pipe(
+      map((result: any) => {
+        return result._embedded.employees;
+      })
+    )
+  }
 }
