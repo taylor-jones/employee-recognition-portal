99e01b8672e8ab62d1dcf2f0a8775060f2f5d181
Taylor Rietz <tmrietz@gmail.com>
2019-07-18 21:34:31 -0500

    Rietz/signature features (#7)
    * Create initial canvas component and services
      - Add a SignatureController class
      - Add a POST route to accept signature data and generate files
      - Create initial UI for canvas component
    
    * Actually commit what I said I did in the last commit
    
    * Fix merge conflicts and change api path

---
+++ erp-client/src/app/services/canvas/canvas.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export class CanvasService {
+
+  private headers: HttpHeaders = null;
+  private endpointBase: string = null;
+
+  constructor(private httpClient: HttpClient) { 
+    this.endpointBase = '/testing'; 
+    this.headers = new HttpHeaders({'Content-Type': 'application/json'});
+  }
+
+  addSignature(imgBase64: string): Observable<string> {
+    return this.httpClient.post<string>('/api/signatures', {"signature": imgBase64}, {headers: this.headers});
+  }
+
+}
