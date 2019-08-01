bb7354059347b993b89d655bc1292628a09fd828
Taylor Jones <jonest6@oregonstate.edu>
2019-07-31 23:32:51 -0500

    Updated sorting functionality to handle objects

--- erp-client/src/app/components/award/award-table/award-table.component.ts
+++ erp-client/src/app/components/award/award-table/award-table.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild, NgModule } from '@angular/core';
 import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 import { AwardService } from 'src/app/services/award/award.service';
-import { ConvertTimePipe } from '../../../pipes/convert-time.pipe';
 
 
 @Component({
@@ -15,7 +14,7 @@ export class AwardTableComponent implements OnInit {
 
 	awards: any = null;
 	errorMessage: string;
-	displayedColumns: string[] = ['id', 'awardType', 'employee', 'awardedDate', 'awardedTime' ];
+	displayedColumns: string[] = ['id', 'awardType', 'employee', 'awardedDate', 'awardedTime', 'edit', 'delete' ];
 	pageSizeOptions: number[];
 	pageSize: number;
 	length: number;
@@ -25,18 +24,27 @@ export class AwardTableComponent implements OnInit {
 
 	ngOnInit() {
     this.getAllAwards();
-	}
+  }
+  
+  ngAfterViewInit (){
+    this.dataSource.sort = this.sort;
+  }
 
 	getAllAwards(): void {
 		this.awardService.getAllAwards().subscribe(
 			(awards) => {
 				this.awards = awards;
 				this.length = awards.length;
-				this.dataSource = new MatTableDataSource(this.awards);
+        this.dataSource = new MatTableDataSource(this.awards);
+        this.dataSource.sortingDataAccessor = (item, property) => {
+          switch(property) {
+            case 'awardType': return item.awardType.name.toLocaleLowerCase();
+            case 'employee': return item.employee.firstName.toLocaleLowerCase();
+            default: return item[property];
+          }
+        };      
 				this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-        
-        console.log(this.awards);
+        this.dataSource.sort = this.sort; 
 			},
 			(error) => {
 				this.errorMessage = 'Failed to load awards';
@@ -44,10 +52,21 @@ export class AwardTableComponent implements OnInit {
 		);
 	}
 
+  // TODO: Make this work w/ employee names and award types
 	public filter(value: string) {
     this.dataSource.filter = value.trim().toLocaleLowerCase();
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
-	}
+  }
+  
+
+  handleEdit(id) {
+    console.log('edit award', id);
+  }
+
+
+  handleDelete(id) {
+    console.log('delete award', id);
+  }
 }
