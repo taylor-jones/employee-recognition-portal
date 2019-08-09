c9b40ec157da876871c55851ff7dd226c907b528
Taylor Jones <jonest6@oregonstate.edu>
2019-08-08 23:29:03 -0500

    Added region/employee/award reports

--- erp-client/src/app/components/report/report.component.ts
+++ erp-client/src/app/components/report/report.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+import { concat, of, throwError, forkJoin, Observable } from 'rxjs';
+import { concatAll } from 'rxjs/operators';
 
 import { ReportService } from 'src/app/services/report/report.service';
+import { AwardTypeService } from 'src/app/services/awardType/awardType.service';
 import { SnackbarService } from 'src/app/services/snackbar/snackbar.service';
 import { User } from 'src/app/models/user.model';
 import { AwardType } from 'src/app/models/awardType.model';
@@ -11,13 +14,13 @@ import { ValidateDate } from 'src/app/validators/date.validator';
 import { ValidateTime } from 'src/app/validators/time.validator';
 
 
-
 @Component({
   selector: 'erp-report',
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.scss']
 })
 export class ReportComponent implements OnInit {
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
 
   //
   // cookies
@@ -27,13 +30,77 @@ export class ReportComponent implements OnInit {
   userId: number;
 
   //
-  // reports
+  // report data
   //
-  awardTypeCounts: null;
-  userAwardCounts: null;
-  regionAwardCounts: null;
-  employeeAwardCounts: null;
-  employeeAwardDiversity: null;
+  awardTypeCounts: any[];
+  userAwardCounts: any[];
+  regionAwardCounts: any[];
+  employeeAwardCounts: any[];
+  employeeAwardDiversity: any[];
+  regionAwards: any = {};
+  regionEmployees: any = {};
+  regionEmployeeAwards: any = {};
+
+  // 
+  // report table
+  // 
+  employeeAwards = new MatTableDataSource([]);
+  displayedColumns: string[] = [
+    'id',
+    'description',
+    'awardedDate',
+    'awardedTime',
+  ];
+
+  // user selections from the charts
+  selected: any = {
+    region: {
+      id: null,
+      name: null,
+    },
+    employee: {
+      id: null,
+      name: null,
+    },
+    awardType: {
+      id: null,
+      name: null,
+    }
+  }
+
+
+  // 
+  // report chards / graphs
+  // 
+  regionAwardTotalsChart = {
+    data: [],
+    showXAxis: true,
+    showYAxis: true,
+    gradient: false,
+    showLegend: true,
+    showXAxisLabel: true,
+    showYAxisLabel: true,
+    xAxisLabel: 'Region',
+    yAxisLabel: 'Total Awards',
+    scheme: 'cool',
+    showAll: true,
+  };
+
+  regionEmployeeAwardsChart = {
+    data: [],
+    gradient: false,
+    showXAxis: true,
+    showYAxis: true,
+    showLegend: true,
+    showXAxisLabel: true,
+    showYAxisLabel: true,
+    xAxisLabel: 'Total Awards',
+    yAxisLabel: 'Employee',
+    scheme: 'cool',
+    showAll: true,
+  };
+
+
 
 
   constructor(
@@ -46,19 +113,25 @@ export class ReportComponent implements OnInit {
     this.cookieUser = cookies['user'];
     this.isAdmin = cookies['admin'] == 'true';
     this.userId = cookies['userId'] || 0;
+
+    Object.assign(this.regionAwardTotalsChart, this.regionAwardCounts);
   }
 
   ngOnInit() {
-    this.getUserAwardCounts();
     this.getRegionAwardCounts();
-    this.getAwardTypeCounts();
-    this.getEmployeeAwardCounts();
-    this.getEmployeeAwardDiversity();
+    // this.getUserAwardCounts();
+    // this.getAwardTypeCounts();
+    // this.getEmployeeAwardCounts();
+    // this.getEmployeeAwardDiversity();
+    this.regionAwardTotalsChart['data'] = [...this.regionAwardTotalsChart['data']];
   }
 
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    this.employeeAwards.sort = this.sort;
+  }
 
-  // TODO: If time, abstract these snackbar functions to somewhere
-  // nices, since they're duplicates of what's in the award component.
 
   /**
    * Shows a snackbar with a success message
@@ -80,6 +153,125 @@ export class ReportComponent implements OnInit {
       duration: 3000,
       panelClass: [ 'snackbar-error' ],
     });
+  }
+
+
+
+  /************************
+   *  Report Charts/Graphs
+   ************************/
+
+  onRegionAwardsChartSelect($event) {
+    const regionId = this.regionAwardCounts.find(region => region.name === $event.name).id;
+    this.selected = {
+      region: {
+        id: regionId,
+        name: $event.name
+      },
+      employee: {
+        id: null,
+        name: null,
+      },
+      awardType: {
+        id: null,
+        name: null,
+      }
+    };
+
+    this.getRegionAwards(regionId);
+  }
+
+
+  onRegionAwardsSlideToggleChange(isChecked) {
+    if (isChecked) {
+      this.regionAwardTotalsChart['data'] = this.regionAwardCounts;
+    } else {
+      this.regionAwardTotalsChart['data'] = this.regionAwardCounts
+        .filter(region => region.value > 0);
+    }
+  }
+
+
+  /**
+   * Builds a list of the number and type of awards for the 
+   * employees at the selected region.
+   * @param regionId id of the selected region
+   */
+  buildRegionEmployeeAwardsHeatMap(regionId) {
+    // if the region awards have already been determined, use those..
+    if (this.regionEmployeeAwards[regionId]) {
+      this.regionEmployeeAwardsChart['data'] = this.regionEmployeeAwards[regionId];
+      return;
+    }
+
+    // if the region employee awards data hasn't yet been gathered...
+    const ra = this.regionAwards[regionId];
+    const re = this.regionEmployees[regionId];
+    const data = [];
+
+    re.forEach((e) => {
+      const name = `${e.firstName} ${e.lastName}`;
+
+      // build the series of awards for this employee
+      const series = [];
+      
+      /**
+       * Filter all of this region's awards to only those that belong
+       * this this employee. Then check this employee's series of awards
+       * to see if this award type is already accounted for. If it is,
+       * then just increment the total count of this award type for this
+       * employee. If this employee does not have any of this award type
+       * yet, then add this award type to the series of awards for this
+       * employee.
+       */
+      ra.filter((a) => a.employee.id === e.id).forEach((b) => {
+        const ex = series.findIndex(obj => obj.name === b.awardType.name);
+        if (ex > -1) {
+          series[ex].value += 1;
+        } else {
+          series.push({
+            name: b.awardType.name,
+            value: 1,
+          });
+        }
+      });
+      
+      // push the series of awards to the region employee awards data
+      if (series.length > 0) {
+        data.push({ 
+          name: name,
+          series: series,
+        });
+      }
+    });
+
+    // update the cached awards list for this region's employees so
+    // that we only need to do this process once.
+    this.regionEmployeeAwards[regionId] = data;
+    this.regionEmployeeAwardsChart['data'] = this.regionEmployeeAwards[regionId];
+  }
+
+
+ /**
+  * Builds a list of award details for the selected
+  * employee and award type.
+  * @param $event the selected chart element
+  */
+  onRegionEmployeeAwardsChartSelect($event) {
+    // find the associated awards for this employee 
+    // and display the details.
+    this.selected.employee.name = $event.series;
+    this.selected.awardType.name = $event.label;
+    
+    this.selected.employee.id = (this.regionEmployees[this.selected.region.id])
+      .find(e => e.fullName === this.selected.employee.name).id;
+    
+    this.employeeAwards = new MatTableDataSource(this
+      .regionAwards[this.selected.region.id]
+      .filter(a => a.employee.id === this.selected.employee.id 
+        && a.awardType.name === this.selected.awardType.name)); 
+
+    this.employeeAwards.sort = this.sort;
   }
 
 
@@ -149,7 +341,41 @@ export class ReportComponent implements OnInit {
 
 
   /**
-   * Fetch the award type count totals.
+   * Fetch the awards for a particular region.
+   */
+  getRegionAwards(regionId: number) {
+    if (this.regionAwards[regionId]) {
+      this.getRegionEmployees(regionId);
+    }
+
+    this._reportService.getRegionAwards(regionId).subscribe(response => {
+      this.regionAwards[regionId] = response;
+      this.getRegionEmployees(regionId);
+    }, err => {
+      this.showSnackbarError('Failed to get region awards.');
+    });
+  }
+
+
+  /**
+   * Fetch the employees from a particular region.
+   */
+  getRegionEmployees(regionId: number) {
+    if (this.regionEmployees[regionId]) {
+      this.buildRegionEmployeeAwardsHeatMap(regionId);
+    }
+
+    this._reportService.getRegionEmployees(regionId).subscribe(response => {
+      this.regionEmployees[regionId] = response;
+      this.buildRegionEmployeeAwardsHeatMap(regionId);
+    }, err => {
+      this.showSnackbarError('Failed to get region awards.');
+    });
+  }
+
+
+  /**
+   * Fetch the total awards for a particular region.
    */
   getRegionAwardCounts() {
     this._reportService.getRegionAwardCounts().subscribe(response => {
@@ -157,11 +383,11 @@ export class ReportComponent implements OnInit {
         return {
           id: item[0],
           name: item[1],
-          total: item[2],
+          value: item[2],
         };
       });
 
-      console.log(this.regionAwardCounts);
+      this.regionAwardTotalsChart['data'] = this.regionAwardCounts;
     }, err => {
       this.showSnackbarError('Failed to get region award totals.');
     });
